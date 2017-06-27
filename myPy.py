413683587b7173dda0b7a267d3ef70b4108e0a87
HP840 <evaseemefly@126.com>
2017-06-27 09:15:23 +0800

    CasablancaPython_Pro 首次创建

---
+++ myPy.py
@@ -0,0 +1,26 @@
+a = "123"
+print(a)
+
+# 有序集合
+mylist = ['联系人1','联系人2','联系人3']
+print(mylist)
+len(mylist)
+# 取出末尾的元素
+a = mylist.pop()
+print(a)
+
+# 元组-tuple
+mytuple=('测试1','测试2','测试3')
+print(mytuple[0])
+
+L=[
+    ['Apple','Google','Microsoft'],
+    ['Java','Python','Ruby','Php'],
+    ['drno','Casablanca']
+]
+# 打印Apple
+print(L[0][0])
+# 打印Python
+print(L[1][1])
+# 打印Casablanca
+print(L[2][1])
