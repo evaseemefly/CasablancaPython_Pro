a27ec8c3fe0209112ebfc727f28d437e34b2ba2f
HP840 <evaseemefly@126.com>
2017-06-29 22:33:29 +0800

    6月29日 家中添加循环和字典

---
+++ py基础-循环.py
@@ -0,0 +1,18 @@
+name=['张三','李四','王五']
+for n in name:
+    print('hello'+n)
+
+arry_int=range(50)
+for temp in arry_int:
+    print(temp)
+
+for temp in range(50):
+    print(temp)
+
+sum=0
+n=99
+while n>0:
+    sum=sum+n
+    n=n-2
+print(sum)
+
---
+++ 使用字典和set.py
@@ -0,0 +1,6 @@
+# d={'name1':90,'name2':80,'name3':70}
+# print(d['name1'])
+
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+d['Michael']
+isExist='Bob' in d
