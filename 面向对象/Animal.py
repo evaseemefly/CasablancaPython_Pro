e65f9b36eae40bb5ac813ef0c6170d75d1d1291f
casablanca <evaseemefly@126.com>
2017-12-31 19:26:35 +0800

    添加了部分内容

--- 面向对象/Animal.py
+++ 面向对象/Animal.py
@@ -6,8 +6,15 @@ class Animal:
     def eat(self):
         print("我正在吃饭，我是%s"%self.name)
 
+    def howoldru(self):
+        # 父类无法访问子类的私有方法或属性
+        print(self.__age)
+        # print(self.age)
+
     def __sweep(self):
         print("我正在梳理毛发")
+
+    
 
 class Cat(Animal):
     def __init__(self,name):
@@ -17,6 +24,10 @@ class Cat(Animal):
     def fathereat(self):
         print("准备调用父类的吃饭方法")
         self.eat()
+    
+    @property
+    def __age(self):
+        return 
 
     def fathersweep(self):
         '''
@@ -28,7 +39,8 @@ class Cat(Animal):
 def main():
     cat=Cat('kimi')
     cat.fathereat()
-    cat.fathersweep()
+    # cat.fathersweep()
+    cat.howoldru()
 
 
 if __name__ == '__main__':
